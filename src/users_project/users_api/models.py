6f32617c0f281e38bfd9b9c94201721c911375e2
Glorius One <m.stoicic@gmail.com>
2019-02-06 20:58:57 +0100

    Created a custom user model and database migrations

--- src/users_project/users_api/models.py
+++ src/users_project/users_api/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+class UsersProfileManager(BaseUserManager):
+  """Helps working with costume user model"""
+
+  def create_user(self, email, name, password=None):
+    """Creates a new user profile object"""
+
+    # Checks if email is entered
+    if not email:
+      raise ValueError('Email adress is required.')
+
+    email = self.normalize_email(email) # Normalizes email and creates new object
+    user = self.model(email=email, name=name)
+
+    # Hides a password
+    user.set_password(password)
+    user.save(using=self._db)
+
+    return user
+
+
+  def create_superuser(self, email, name, password):
+    """Creates a new superuser"""
+
+    user = self.create_superuser(email, name, password)
+
+    user.is_superuser = True
+    user.save(using=self._db)
+
+    return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+  """Represents user profile in our system"""
+
+  email = models.EmailField(max_length=255, unique=True)
+  name = models.CharField(max_length=255)
+  is_active = models.BooleanField(default=True)  # Determins if user is currently active in system
+  is_staff = models.BooleanField(default=False)
+
+  object = UsersProfileManager()
+
+  USERNAME_FIELD = 'email'
+  # List of required fields
+  REQUIRED_FIELDS = ['name']
+
+
+  def get_full_name(self):
+    """Gets user full name"""
+
+    return self.name
+
+  def get_short_name(self):
+    """"Gets users short name"""
+
+    return self.name
+
+
+  def __str__(self):
+    """"Represents object as a string"""
+
+    return self.email
--- src/users_project/users_project/settings.py
+++ src/users_project/users_project/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Assigned costum user model
+AUTH_USER_MODEL = 'users_api.UserProfile'
