e78a878e56355b3766ddeadb7b94a6ce8f052cdb
Glorius One <m.stoicic@gmail.com>
2019-02-09 00:40:14 +0100

    Added deploy files, removed debug

--- src/users_project/users_project/settings.py
+++ src/users_project/users_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2y*%wlz*om^v55-)c#(&tj7gjrxch(%w#yg6&5u^*%4-psgji5'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
